adb586ac2d1c1e8219d63adac99cba5eb7df4d0c
Jonas <mz-martinez@live.com>
2022-05-05 15:20:24 -0300

    Stats update

--- stats.js
+++ stats.js
@@ -4,7 +4,3 @@ const { freemem, totalmem } = os;
 
 console.log(`Livre: ${parseInt(freemem() / 1024 / 1024)} MB`, `Total: ${parseInt(totalmem() / 1024 / 1024)} MB`, `Usage: ${parseInt((freemem/totalmem)*100)}% `);
 
-console.log(`
-
-
-${JSON.stringify(os.cpus(), null, 2)}`);
