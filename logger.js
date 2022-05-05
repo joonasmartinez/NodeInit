3b6188b8ffcb8462c3e5424c6e39c36493ed191f
Jonas <mz-martinez@live.com>
2022-05-05 15:41:23 -0300

    Log added

--- logger.js
+++ logger.js
@@ -2,6 +2,12 @@ const EventEmitter = require('events'); // EventEmitter requer modulo events
 
 const emitter = new EventEmitter(); //EventEmitter é uma Classe do modulo e 'emitter' agora é uma instancia de EventEmitter (Traz Atributos e Métodos).
 
-emitter.on('log', ()=>{
+emitter.on('log', (message)=>{
     console.log(message);
 })
+
+function log(message){
+    emitter.emit('log', message);
+}
+
+module.exports = log;
--- stats.js
+++ stats.js
@@ -1,4 +1,5 @@
 const os = require('os');
+const log = require('./logger')
 
 setInterval(()=>{
 
@@ -16,6 +17,8 @@ setInterval(()=>{
     console.clear();
     console.log("====== PC STATS ======")
     console.table(stats);
+
+    log('Rodando...')
 
 }, 1000)
 
