b996b72238ec51fa4d41cb24a8581348c149e62a
Jonas <mz-martinez@live.com>
2022-05-05 21:31:42 -0300

    Log concluído

--- logger.js
+++ logger.js
@@ -1,13 +1,15 @@
 const EventEmitter = require('events'); // EventEmitter requer modulo events
+const fs = require('fs'); //Trabalhar com arquivos do sistema.
+const path = require('path');
 
 const emitter = new EventEmitter(); //EventEmitter é uma Classe do modulo e 'emitter' agora é uma instancia de EventEmitter (Traz Atributos e Métodos).
 
 emitter.on('log', (message)=>{
-    console.log(message);
+    fs.appendFile(path.join(__dirname, 'log.txt'), message, err=>{
+        if (err) throw err
+    });
 })
-
 function log(message){
     emitter.emit('log', message);
 }
-
 module.exports = log;
